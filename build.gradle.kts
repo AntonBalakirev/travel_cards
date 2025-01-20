adfc9c79556a931cdd7d9a047f39623a3218acc5
anton_balakirev <anton_balakirev@epage.ru>
2025-01-20 23:49:31 +0100

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+kotlin {
+    jvmToolchain(17)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.h2database:h2:2.2.224")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(kotlin("test"))
+}
+
+plugins {
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
+}
+
+tasks.bootRun {
+    mainClass.set("org.example.travelcards.TravelCardsApplicationKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
